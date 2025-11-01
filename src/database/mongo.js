44b1a040e981f68eeed03f4bd740633201ad481d
nk1433 <iamnk1433@gmail.com>
2025-11-01 21:07:31 +0530

    Dusted.

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/trade-dashboard';
-console.log(MONGODB_URI, 'MONGODB_URI')
 
 class MongoConnection {
   constructor() {
