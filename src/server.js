bb9841379b94ae1126b6d143fb10ab1003e40806
nk1433 <iamnk1433@gmail.com>
2025-11-02 14:37:21 +0530

    Dusted.

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import http from "http";
 import app from "./app.js";
 import setupWebSocket from "./ws/server.js";
 
-const PORT = process.env.PORT || 3015;
+const PORT = 3015;
 
 const server = http.createServer(app);
 
